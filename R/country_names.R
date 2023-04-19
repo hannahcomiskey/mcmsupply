aefb1664e590e31cb801e7ac31667dc3dd417c1b
hcomiskey <hcomiskey@rstudioserver.hamilton.ie>
2023-04-19 15:10:27 +0100

    Updates for country_names

---
+++ R/country_names.R
@@ -0,0 +1,11 @@
+#' The names of the countries with national and subnational administration level data stored
+#'
+#' @docType data
+#' @keywords country_names
+#' @format  A data frame with 30 rows and 3 columns:
+#' \describe{
+#'   \item{Country name}{Country name}
+#'   \item{National level data available}{Indicator for whether data at the national administration level is available in the package}
+#'   \item{Subnational level data available}{Indicator for whether data at the subnational administration level is available in the package}
+#' }
+"country_names"
