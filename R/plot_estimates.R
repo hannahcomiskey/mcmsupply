67322e73dceba224e5ce65e25e03231494c05d1b
hcomiskey <hcomiskey@rstudioserver.hamilton.ie>
2023-04-20 10:45:12 +0100

    Updates to reduce number of arguments in functions

--- R/plot_estimates.R
+++ R/plot_estimates.R
@@ -1,11 +1,9 @@
 #' Wrapper function to plot the JAGS estimates
 #' @name plot_estimates
-#' @param national TRUE/FALSE. Default is TRUE for national administration level data. FALSE retrieves subnational level data.
 #' @param main_path String. Path where you have set your model results to be saved to.
 #' @param vis_path String. Path where your visualisations will be saved to.
 #' @param jagsdata Output of the `mcmsupply::get_modelinputs()` function.
-#' @param local TRUE/FALSE. Default is FALSE. local=FALSE retrieves the data for all subnational provinces across all countries. local=TRUE retrieves data for only one country.
-#' @param mycountry The country name of interest in a local run. You must have local=TRUE for this functionality. A list of possible countries available found in data/mycountries.rda.
+#' @param ... Arguments from the `mcmsupply::get_modelinputs()` function.
 #' @examples National single-country example:
 #' plot_estimates(main_path = "results/test", vis_path  = "visualisations/test", jagsdata, ...)
 #'
--- R/run_jags_model.R
+++ R/run_jags_model.R
@@ -1,14 +1,12 @@
 #' Wrapper function to run the jags model for estimating the proportion of modern contraceptive methods supplied by the public & private Sectors using a Bayesian hierarchical penalized spline model for the national and subnational administration levels
 #' @name run_jags_model
-#' @param national TRUE/FALSE. Default is TRUE for national administration level data. FALSE retrieves subnational level data.
 #' @param jagsdata The inputs for the JAGS model
 #' @param jagsparams The parameters of the JAGS model you wish to review
-#' @param local TRUE/FALSE. Default is FALSE. local=FALSE retrieves the data for all subnational provinces across all countries. local=TRUE retrieves data for only one country.
 #' @param main_path Default is "results/". String to indicate where to save results.
 #' @param n_iter Default is 80000. Number of itterations to do in JAGS model.
 #' @param n_burnin Default is 10000. Number of samples to burn-in in JAGS model.
 #' @param n_thin Default is 35. Number of samples to thin by in JAGS model.
-#' @param mycountry The country name of interest in a local run. You must have local=TRUE for this functionality. A list of possible countries available found in data/mycountries.rda.
+#' @param ... Argumentsfrom the `mcmsupply::get_modelinputs()` function.
 #' @return returns the jags model object
 #' importFrom("stats", "cor", "filter", "lag")
 #' @import R2jags runjags tidyverse tidybayes
