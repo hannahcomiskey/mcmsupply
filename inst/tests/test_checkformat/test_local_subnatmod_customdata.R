5caf8bb14c671ea6cedff6bc0907ed663f32dbad
Hannah Comiskey <hannahcomiskey68@gmail.com>
2023-06-23 13:33:53 +0100

    remove check_format data

--- inst/tests/test_checkformat/test_local_subnatmod_customdata.R
+++
@@ -1,8 +0,0 @@
-# ### How to run a subnational level model for a single country with custom data
-# devtools::load_all()
-# library(dplyr)
-# cleaned_data <- get_data(national=FALSE, local=TRUE, mycountry="Congo Democratic Republic", fp2030=TRUE, surveydata_filepath="data-raw/sample_custom_datasets/DRC_admin2_FPsource.xlsx")
-# pkg_data <- get_modelinputs(startyear=1990, endyear=2025.5, nsegments=12, raw_data = cleaned_data)
-# run_jags_model(jagsdata = pkg_data, jagsparams = NULL, main_path = "results/subnational/local/test/", n_iter = 80000, n_burnin = 10000, n_thin = 35)
-# plot_estimates(main_path="results/subnational/local/test/", vis_path="visualisation/subnational/local/test/", pkg_data)
-#
