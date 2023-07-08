ca3f266909b2526515c00c3cb859ef24d4607bb5
Hannah Comiskey <hannahcomiskey68@gmail.com>
2023-07-08 17:50:24 +0100

    updates

--- R/tau_alphapms_subnationalmod.R
+++ R/tau_alphapms_subnationalmod.R
@@ -1,3 +1,4 @@
+#' tau_alphapms_subnationalmod
 #' The median estimates of the precision for the subnational-level country, sector-, method-specific intercepts in the multi-country subnational model.
 #' This vector is used to inform the precision in the Normal prior of the subnational-level intercept in single-country subnational models.
 #'
