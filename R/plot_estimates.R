af6a9c27a0ffb743ad00cb41af5e82ae88446f1a
Hannah Comiskey <hannahcomiskey68@gmail.com>
2023-07-10 17:27:44 +0100

    updates

--- R/plot_estimates.R
+++ R/plot_estimates.R
@@ -3,7 +3,7 @@
 #' @param jagsdata Output of the mcmsupply::get_modelinputs() function.
 #' @param model_output The output of the mcmsupply::run_jags_model() function.
 #' @examples
-#' \dontrun{
+#' \donttest{
 #' raw_data <- get_data(national=TRUE, local=TRUE, mycountry="Nepal")
 #' jagsdata <- get_modelinputs(startyear=1990, endyear=2030.5, nsegments=12, raw_data)
 #' mod <- run_jags_model(jagsdata, n_iter=5, n_burnin=1, n_thin=1)
