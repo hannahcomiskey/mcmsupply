eb3f9e3a3e0ee83765430309bbc673161a804d48
Hannah Comiskey <hannacomiskey68@gmail.com>
2024-03-04 11:26:36 +0000

    updates to data documentation

---
+++ R/national_FPsource_VARCOV_bivarlogitnormal.R
@@ -0,0 +1,7 @@
+#' An array of variance-covariance matrices corresponding to the DHS survey observations for the proportion of modern contraceptives supplied by the public and private sectors at the national level
+#'
+#' @format ## `national_FPsource_VARCOV_bivarlogitnormal`
+#' An array of 2x2 matrices for each of the 487 observations in the national_FPsource_data.
+#' Each 2x2 array corresponds to the variance of the public and private sectors on the diagonal and their corresponding covariances on the off-diagonal.
+#' @source The variance-covariance matrices are calculated using the svyVGAM::svy_vglm() function.
+"national_FPsource_VARCOV_bivarlogitnormal"
